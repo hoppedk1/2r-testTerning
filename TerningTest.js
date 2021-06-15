11a7788c55701ec15395c146b393a8915ed8a196
hoppedk1 <73273792+hoppedk1@users.noreply.github.com>
2021-06-15 13:29:25 +0200

    Add files via upload

---
+++ TerningTest.js
@@ -0,0 +1,14 @@
+
+var terning1 = 0;
+var terning2 = 0;
+var terning3 = 0;
+function kast(){
+terning1 = Math.floor(Math.random()*6+1);
+terning2 = Math.floor(Math.random()*6+1);
+terning3 = Math.floor(Math.random()*6+1);
+
+
+console.log(kast(terning1))
+}
+console.log(kast(terning1))
+console.log(Math.floor(Math.random()*6+1));
